d845688bee92fa7a6b0a472e27552c7e6caaebaa
Filiperodrigues-a <166249999+Filiperodrigues-a@users.noreply.github.com>
2024-10-01 22:20:19 -0300

    Delete aula3.2_questao3.py

--- aula3.2_questao3.py
+++
@@ -1,9 +0,0 @@
-idade = int(input("Informe sua idade:"))
-partidas = input("Já jogou pelo menos 3 jogos de tabuleiro?")
-vitorias = int(input("Quantos jogos já venceu?"))
-apto_idade = idade>=16 and idade<=18
-apto_partida = partidas == 'True'
-apto_vitorias = vitorias>=1
-apto = apto_idade and apto_partida and apto_vitorias
-
-print(f"Apto para ingressar no clube de jogos de tabuleiro:{apto}")
