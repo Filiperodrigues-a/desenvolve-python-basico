0ea744fd18024bbda7c3be44890767c32605e425
Filiperodrigues-a <166249999+Filiperodrigues-a@users.noreply.github.com>
2024-10-01 22:12:04 -0300

    Delete aula2.4_questao2.py

--- aula2.4_questao2.py
+++
@@ -1,6 +0,0 @@
-t_fahrenheit = int(input("Informe a temperatura em Fahrenheit:"))
-#formula de conversão Fahrenheit para Celsius 
-t_celsius = (t_fahrenheit-32)*(5/9)
-#conversão de número flutuante para número inteiro
-t_celsius = int(t_celsius)
-print(f"{t_fahrenheit} graus Fahrenheit são {t_celsius} graus Celsius.")
