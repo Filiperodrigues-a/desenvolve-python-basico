3fed29b43e58cd9ebc42833284302a8c1272afa0
Filiperodrigues-a <166249999+Filiperodrigues-a@users.noreply.github.com>
2024-10-01 22:19:40 -0300

    Delete aula3.2_questao2.py

--- aula3.2_questao2.py
+++
@@ -1,4 +0,0 @@
-idade_juliana = int(input("Informe a idade de Juliana:"))
-idade_cris = int(input("Informe a idade de Cris:"))
-pode_entrar = idade_juliana>17 or idade_cris>17
-print(f"Elas poderam entrar:{pode_entrar}")
