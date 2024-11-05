ea645cf95ebece82f552998d17fbc69297253497
Filiperodrigues-a <166249999+Filiperodrigues-a@users.noreply.github.com>
2024-11-04 23:41:31 -0300

    Add files via upload

---
+++ aula1_questao1.py
@@ -0,0 +1,5 @@
+n1 = float(input("Informe o primeiro número decimal:"))
+n2 = float(input("Informe o segundo número decimal:"))
+n3 = abs(n1) - abs(n2)
+n3 = round(n3,2)
+print(f"A diferença absoluta entre os números é:{n3}")
