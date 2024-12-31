752b5d2e21dcfa0709c38903bf463744515d4386
Filiperodrigues-a <166249999+Filiperodrigues-a@users.noreply.github.com>
2024-12-31 00:12:44 -0300

    Delete aula4_questao3.py

--- aula4_questao3.py
+++
@@ -1,9 +0,0 @@
-n1, n2, n3 = int(input("Informe a 1° nota:")), int(input("Informe a 2° nota:")), int(input("Informe a 3° nota,:")),
-m = (n1+n2+n3)/3
-if m >= 60:
-    print("Aprovado")
-elif m >= 40:
-    print("Recuperação")
-else:
-    print("Reprovado")
-print("Fim")
